cef5069a8e45b71d870953f84a1d575a40a32f60
Deepak Verma <deepakverma@abaq.us>
2024-07-09 00:18:00 +0530

    features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
-import './App.css'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
-import Model from './components/Model'
+import Model from './components/Model';
+import Features from './components/Features';
+// import HowItWorks from './components/HowItWorks';
+// import Footer from './components/Footer';
 
+import * as Sentry from '@sentry/react';
 
-export default function App() {
+const App = () => {
   return (
-    <main className='bg-black'>
+    <main className="bg-black">
       <Navbar />
       <Hero />
       <Highlights />
-      <Model/>
+      <Model />
+      <Features />
+      {/* <HowItWorks />
+      <Footer /> */}
     </main>
   )
 }
 
+export default Sentry.withProfiler(App);
